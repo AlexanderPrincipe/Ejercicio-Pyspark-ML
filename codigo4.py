74868a3d5154e97df4c9434cc4f151962a866133
Alexander Principe <principetolentinoa@hotmail.com>
2019-07-08 19:55:55 -0500

    Corregido

--- codigo4.py
+++ codigo4.py
@@ -1,12 +1,12 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
 from pyspark.ml import Pipeline
-from pyspark.ml.feature import VectorAssembler, StringIndexer
+from pyspark.ml.feature import VectorAssembler, StringIndexer, Normalizer
 from pyspark.ml.feature import VectorIndexer, ChiSqSelector		
 from pyspark.ml.classification import DecisionTreeClassifier
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
-
+# Convertir a entero jugadores ofensivos y jugadores defensivos
 def tipo_posiciones(p):
     # Ofensivo
     if p == 'RF' or p == 'ST' or p == 'LF' or p == 'RS' or p == 'LS' or p == 'CF' or p == 'LW' or p == 'RCM' or p == 'LCM' or p == 'LDM' or p == 'CAM' or p == 'CDM' or p == 'RM' or p == 'LAM' or p == 'LM' or p == 'RDM' or p == 'RW' or p == 'CM' or p == 'RAM':
@@ -17,51 +17,63 @@ def tipo_posiciones(p):
     else:
         return 1
 
+# Cargar el CSV
 def leer_df():
 	conf = SparkConf().setAppName("Tarea4").setMaster("local")
 	sc = SparkContext(conf=conf)
 
 	sqlContext = SQLContext(sc)
+	
+	# Creacion de rdd
+	rdd = sqlContext.read.csv("data.csv", header=True).rdd
 
-	# Leemos el CSV
-	rdd = sqlContext.read.csv("data_real.csv", header=True).rdd
+	# Filtrando datos vacios
+	rdd = rdd.filter(
+		lambda x: (		   x[21] != None and x[21] != '' and x[54] != None and x[55] != None and x[56] != None and \
+						   x[57] != None and x[58] != None and x[59] != None and x[60] != None and x[61] != None and x[62] != None and x[63] != None and x[64] != None and x[65] != None and x[66] != None and \
+						   x[67] != None and x[68] != None and x[69] != None and x[70] != None and x[71] != None and x[72] != None and x[73] != None and x[74] != None and x[75] != None and x[76] != None and \
+						   x[77] != None and x[78] != None and x[79] != None and x[80] != None and x[81] != None and x[82] != None and x[83] != None and x[84] != None and x[85] != None and x[86] != None and x[87] != None ))
 
+	# Consideramos los principales features
 	rdd = rdd.map(
-        lambda x: (int(x[54]),int(x[55]),int(x[56]),int(x[57]),int(x[58]), int(x[59]),
-		                  int(x[60]), int(x[61]), int(x[62]),int(x[63]), int(x[64]), int(x[65]) , int(x[66]),
-		                  int(x[67]), int(x[68]), int(x[69]),int(x[70]), int(x[71]), int(x[72]) , int(x[73]),
-		                  int(x[74]), int(x[75]), int(x[76]),int(x[77]), int(x[78]), int(x[79]) , int(x[80]),
-		                  int(x[81]), int(x[82]), int(x[83]),int(x[84]), int(x[85]), int(x[86]) , int(x[87]), tipo_posiciones((x[21]))))
-	df = rdd.toDF(["Crossing","Finishing","HeadingAccuracy","ShortPassing","Volleys","Dribbling","Curve",
+        lambda x: (		  tipo_posiciones((x[21])) ,int(x[54].split('+')[0]),int(x[55].split('+')[0]),int(x[56].split('+')[0]),int(x[57].split('+')[0]),int(x[58].split('+')[0]), int(x[59].split('+')[0]),
+		                  int(x[60].split('+')[0]), int(x[61].split('+')[0]), int(x[62].split('+')[0]),int(x[63].split('+')[0]), int(x[64].split('+')[0]), int(x[65].split('+')[0]) , int(x[66].split('+')[0]),
+		                  int(x[67].split('+')[0]), int(x[68].split('+')[0]), int(x[69].split('+')[0]),int(x[70].split('+')[0]), int(x[71].split('+')[0]), int(x[72].split('+')[0]) , int(x[73].split('+')[0]),
+		                  int(x[74].split('+')[0]), int(x[75].split('+')[0]), int(x[76].split('+')[0]),int(x[77].split('+')[0]), int(x[78].split('+')[0]), int(x[79].split('+')[0]) , int(x[80].split('+')[0]),
+		                  int(x[81].split('+')[0]), int(x[82].split('+')[0]), int(x[83].split('+')[0]),int(x[84].split('+')[0]), int(x[85].split('+')[0]), int(x[86].split('+')[0]) , int(x[87].split('+')[0])))
+	# Convertimos el rdd a df					  
+	df = rdd.toDF(["Position","Crossing","Finishing","HeadingAccuracy","ShortPassing","Volleys","Dribbling","Curve",
                    "FKAccuracy", "LongPassing","BallControl","Acceleration","SprintSpeed","Agility","Reactions",
                    "Balance","ShotPower","Jumping","Stamina","Strength","LongShots","Aggression",
                    "Interceptions","Positioning","Vision","Penalties","Composure","Marking","StandingTackle",
-                   "SlidingTackle","GKDiving","GKHandling","GKKicking","GKPositioning","GKReflexes", "Position"])
+                   "SlidingTackle","GKDiving","GKHandling","GKKicking","GKPositioning","GKReflexes"])
 	return df
 
 
-
+# Analizar con los features mas representativos para el modelo
 def leer_df_categoricos():
 	conf = SparkConf().setAppName("Tarea4").setMaster("local")
 	sc = SparkContext(conf=conf)
 
 	sqlContext = SQLContext(sc)
+	
+	# Creacion de rdd
+	rdd = sqlContext.read.csv("data.csv", header=True).rdd
+	
+	# Filtramos los datos vacios
+	rdd = rdd.filter(
+		lambda x: (		   x[21] != None and x[21] != '' and x[55] != None and x[57] != None and x[63] != None and \
+						   x[71] != None and x[82] != None and x[87] != None and x[54] != None and x[66] != None and x[59] != None and x[65] != None  ))
 
-	# Leemos el CSV
-	rdd = sqlContext.read.csv("data_real.csv", header=True).rdd
-
+	# Features mas representativos
 	rdd = rdd.map(
-        lambda x: (int(x[54]),int(x[55]),int(x[56]),int(x[57]),int(x[58]), int(x[59]),
-		                  int(x[60]), int(x[61]), int(x[62]),int(x[63]), int(x[64]), int(x[65]) , int(x[66]),
-		                  int(x[67]), int(x[68]), int(x[69]),int(x[70]), int(x[71]), int(x[72]) , int(x[73]),
-		                  int(x[74]), int(x[75]), int(x[76]),int(x[77]), int(x[78]), int(x[79]) , int(x[80]),
-		                  int(x[81]), int(x[82]), int(x[83]),int(x[84]), int(x[85]), int(x[86]) , int(x[87]), tipo_posiciones((x[21]))))
-	df = rdd.toDF(["Crossing","Finishing","HeadingAccuracy","ShortPassing","Volleys","Dribbling","Curve",
-                   "FKAccuracy", "LongPassing","BallControl","Acceleration","SprintSpeed","Agility","Reactions",
-                   "ShotPower","Jumping","Stamina","Strength","LongShots",
-                   "Interceptions","Positioning","Vision","Marking","StandingTackle",
-                   "Position"])
+        lambda x: (tipo_posiciones((x[21])) ,int(x[55].split('+')[0]), int(x[57].split('+')[0]), int(x[63].split('+')[0]),
+		int(x[71].split('+')[0]),int(x[82].split('+')[0]), int(x[87].split('+')[0]),int(x[54].split('+')[0]),int(x[66].split('+')[0]),int(x[59].split('+')[0]), int(x[65].split('+')[0])  ))
+	df = rdd.toDF(["Position", "Finishing", "ShortPassing", "BallControl", "Stamina", "SlidingTackle", "GKReflexes", "Crossing", "Agility", "Dribbling", "SprintSpeed"])
 
+	return df
+
+# Seleccionar los features mas representativos para el modelo
 def feature_selection(df):
 	assembler = VectorAssembler(
 		inputCols=["Crossing","Finishing","HeadingAccuracy","ShortPassing","Volleys","Dribbling","Curve",
@@ -74,14 +86,13 @@ def feature_selection(df):
 
 	indexer = VectorIndexer(
 		inputCol="features", 
-		outputCol="indexedFeatures",
-		maxCategories=4)
+		outputCol="indexedFeatures")
 	
 	df = indexer.fit(df).transform(df)
 
-	# Seleccionamos features que mas suman al modelo
+	# Prueba ChiSquare
 	selector = ChiSqSelector(
-		numTopFeatures=2,
+		numTopFeatures=10,
 		featuresCol="indexedFeatures",
 		labelCol="Position",
 		outputCol="selectedFeatures")
@@ -90,9 +101,9 @@ def feature_selection(df):
 
 def entrenamiento(df):
 	# Vectorizo
-	df = df.select("cp", "restecg", "thal", "target")
+	df = df.select("Finishing", "ShortPassing", "BallControl", "Stamina", "SlidingTackle", "GKReflexes", "Crossing", "Agility", "Position", "Dribbling", "SprintSpeed")
 	assembler = VectorAssembler(
-		inputCols=["cp","restecg","thal"],
+		inputCols=["Finishing", "ShortPassing", "BallControl", "Stamina", "SlidingTackle", "GKReflexes", "Crossing", "Agility"],
 		outputCol="features")
 	df = assembler.transform(df)
 
@@ -106,22 +117,23 @@ def entrenamiento(df):
 	pipeline = Pipeline(stages=[entrenador])
 	model = pipeline.fit(training_df)
 
+	# Prediccion
 	predictions_df = model.transform(test_df)
 
 	evaluator = MulticlassClassificationEvaluator(
-		labelCol="target",
+		labelCol="Position",
 		predictionCol="prediction",
 		metricName="accuracy")
 
+	# Exactitud
 	exactitud = evaluator.evaluate(predictions_df)
 	print("Exactitud: {}".format(exactitud))
 
 def main():
 	df = leer_df()
-	feature_selection(df)
-	#entrenamiento(df)
+	#df = leer_df_categoricos()
+	#feature_selection(df)
+	entrenamiento(df)
 	
-
-
 if __name__ == "__main__":
 	main()
